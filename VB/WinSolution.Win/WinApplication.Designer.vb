43bcbfb1ff02f2635e4f332f2984a9759f386b69
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/WinSolution.Win/WinApplication.Designer.vb
@@ -0,0 +1,62 @@
+Imports Microsoft.VisualBasic
+Imports System
+Namespace WinSolution.Win
+	Partial Public Class WinSolutionWindowsFormsApplication
+		''' <summary> 
+		''' Required designer variable.
+		''' </summary>
+		Private components As System.ComponentModel.IContainer = Nothing
+
+		''' <summary> 
+		''' Clean up any resources being used.
+		''' </summary>
+		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+			If disposing AndAlso (components IsNot Nothing) Then
+				components.Dispose()
+			End If
+			MyBase.Dispose(disposing)
+		End Sub
+
+		#Region "Component Designer generated code"
+
+		''' <summary> 
+		''' Required method for Designer support - do not modify 
+		''' the contents of this method with the code editor.
+		''' </summary>
+		Private Sub InitializeComponent()
+			Me.module1 = New DevExpress.ExpressApp.SystemModule.SystemModule()
+			Me.module2 = New DevExpress.ExpressApp.Win.SystemModule.SystemWindowsFormsModule()
+			Me.module3 = New WinSolution.Module.WinSolutionModule()
+			Me.module4 = New WinSolution.Module.Win.WinSolutionWindowsFormsModule()
+			Me.module6 = New DevExpress.ExpressApp.Objects.BusinessClassLibraryCustomizationModule()
+			Me.schedulerModuleBase1 = New DevExpress.ExpressApp.Scheduler.SchedulerModuleBase()
+			Me.schedulerWindowsFormsModule1 = New DevExpress.ExpressApp.Scheduler.Win.SchedulerWindowsFormsModule()
+			CType(Me, System.ComponentModel.ISupportInitialize).BeginInit()
+			' 
+			' WinSolutionWindowsFormsApplication
+			' 
+			Me.ApplicationName = "WinSolution"
+			Me.Modules.Add(Me.module1)
+			Me.Modules.Add(Me.module2)
+			Me.Modules.Add(Me.module6)
+			Me.Modules.Add(Me.module3)
+			Me.Modules.Add(Me.schedulerModuleBase1)
+			Me.Modules.Add(Me.schedulerWindowsFormsModule1)
+			Me.Modules.Add(Me.module4)
+'			Me.DatabaseVersionMismatch += New System.EventHandler(Of DevExpress.ExpressApp.DatabaseVersionMismatchEventArgs)(Me.WinSolutionWindowsFormsApplication_DatabaseVersionMismatch);
+			CType(Me, System.ComponentModel.ISupportInitialize).EndInit()
+
+		End Sub
+
+		#End Region
+
+		Private module1 As DevExpress.ExpressApp.SystemModule.SystemModule
+		Private module2 As DevExpress.ExpressApp.Win.SystemModule.SystemWindowsFormsModule
+		Private module3 As WinSolution.Module.WinSolutionModule
+		Private module4 As WinSolution.Module.Win.WinSolutionWindowsFormsModule
+		Private module6 As DevExpress.ExpressApp.Objects.BusinessClassLibraryCustomizationModule
+		Private schedulerModuleBase1 As DevExpress.ExpressApp.Scheduler.SchedulerModuleBase
+		Private schedulerWindowsFormsModule1 As DevExpress.ExpressApp.Scheduler.Win.SchedulerWindowsFormsModule
+	End Class
+End Namespace
